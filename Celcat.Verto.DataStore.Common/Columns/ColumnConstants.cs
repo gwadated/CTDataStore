f92441ff7b9c93b7a44a46df2703cc9e67177a27
Wayne Si <w.si@celcat.com>
2020-09-04 19:11:08 +0100

    Update to be compatible with v8.2 CT_EVENT.custom1/2/3 field size change (to 256 chars)

--- Celcat.Verto.DataStore.Common/Columns/ColumnConstants.cs
+++ Celcat.Verto.DataStore.Common/Columns/ColumnConstants.cs
@@ -9,7 +9,7 @@
         public const int StrLenStaffStudentTitle = 32;
         public const int StrLenComments = 255; // actually bigger than current CT7 implementation (future proof)
         public const int StrLenDescription = 255; // actually bigger than current CT7 implementation (future proof)
-        public const int StrLenLookup = 255; // actually bigger than current CT7 implementation (future proof)
+        public const int StrLenLookup = 256; // actually bigger than current CT7 implementation (future proof)
         public const int StrLenOriginalId = 255; // actually bigger than current CT7 implementation (future proof)
         public const int StrLenNaturalKey = 255; // actually bigger than current CT7 implementation (future proof)
         public const int StrLenTel = 128;   // actually bigger than current CT7 implementation (future proof)
